fa094b7d095c8fe2e8248ab37f8cf90e0cfdee04
goeti <mv1451003@gmail.com>
2018-12-23 18:23:02 +0400

    1
    Signed-off-by: goeti <mv1451003@gmail.com>

--- in.py
+++ in.py
@@ -2,20 +2,10 @@ import sys
 
 import numpy as np
 import pyaudio as pa
-from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtWidgets import QInputDialog
 
-SAMPLE_RATE = 44100
-#                      0       1       2      3        4       5       6
-#                      до      ре      ми     фа       соль    ля      си
-freq_array = np.array([261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88])
-key_names = ['a', 's', 'd', 'f', 'g', 'h', 'j']
-key_list = list(map(lambda x: ord(x), key_names))
-key_dict = dict([(key, False) for key in key_list])
 
-
-# функции генерации волн
 def square(x):
     return np.clip(np.ceil(np.sin(x)), 0, 0.5)
 
@@ -29,16 +19,8 @@ def triangle(x):
     return np.abs(x % 6 - 3) / (2 * np.pi)
 
 
-gen_func = np.sin
-gen_func_list = (
-    (np.sin, ord('w')),
-    (square, ord('e')),
-    (saw, ord('r')),
-    (triangle, ord('t'))
-)
-
-
 def generate_sample(freq, duration, volume):
+    gen_func = np.sin
     amplitude = np.round((2 ** 16) * volume)
     total_samples = np.round(SAMPLE_RATE * duration)
     w = 2.0 * np.pi * freq / SAMPLE_RATE
@@ -54,10 +36,36 @@ def generate_tones(duration):
     return tones
 
 
-class Example(QWidget):
+class Example(QWidget, ):
     def __init__(self):
         super().__init__()
         self.initUI()
+
+        # функции генерации волн
+
+    def square(self, x):
+        return np.clip(np.ceil(np.sin(x)), 0, 0.5)
+
+    def saw(self, x):
+        c = x / np.pi - 0.5
+        return 0.5 * (c - np.floor(0.5 + c)) + 0.25
+
+    def triangle(self, x):
+        return np.abs(x % 6 - 3) / (2 * np.pi)
+
+    def generate_sample(self, freq, duration, volume):
+        amplitude = np.round((2 ** 16) * volume)
+        total_samples = np.round(SAMPLE_RATE * duration)
+        w = 2.0 * np.pi * freq / SAMPLE_RATE
+        k = np.arange(0, total_samples)
+        return np.round(amplitude * self.gen_func(k * w))
+
+    def generate_tones(self, duration):
+        tones = []
+        for freq in self.freq_array:
+            tone = np.array(generate_sample(freq, duration, 1.0), dtype=np.int16)
+            tones.append(tone)
+        return tones
 
     def initUI(self):
         self.setGeometry(300, 300, 400, 400)
@@ -79,6 +87,29 @@ class Example(QWidget):
         self.button_3.clicked.connect(self.run2)
         self.show()
 
+        self.SAMPLE_RATE = 44100
+        #                      0       1       2      3        4       5       6
+        #                      до      ре      ми     фа       соль    ля      си
+        self.freq_array = np.array([261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88])
+        self.key_names = ['A', 'S', 'D', 'F', 'G', 'H', 'J']
+        self.key_list = list(map(lambda x: ord(x), self.key_names))
+        self.key_dict = dict([(key, False) for key in self.key_list])
+
+        self.freqdict = {"Субконтроктава": self.freq_array / 16, "Контроктава": self.freq_array / 8,
+                         "Большая октава": self.freq_array / 4, 'Малая октава': self.freq_array / 2,
+                         'Первая октава': self.freq_array, 'Вторая октава': self.freq_array * 2,
+                         'Третья октава': self.freq_array * 4, 'Четвёртая октава': self.freq_array * 8,
+                         'Пятая октава': self.freq_array * 16}
+        self.gen_func = np.sin
+        self.gen_func_list = (
+            (np.sin, ord('W')),
+            (square, ord('E')),
+            (saw, ord('R')),
+            (triangle, ord('T'))
+        )
+        self.duration_tone = 1 / 2.0
+        self.tones = generate_tones(self.duration_tone)
+
     def run(self):
         i, okBtnPressed = QInputDialog.getItem(
             self,
@@ -90,19 +121,20 @@ class Example(QWidget):
             False
         )
         if okBtnPressed:
-            self.button_1.setText(i)
+            self.freq_array = self.freqdict[i]
 
     def run1(self):
         i, okBtnPressed = QInputDialog.getItem(
             self,
             "Выберите волну",
             'вы можете менять виды синтезируемых волн на кнопки w, e, r, t',
-            ("sin", "square", "saw", 'triangle'),
+            ("np.sin", "square", "saw", 'triangle'),
             0,
             False
         )
         if okBtnPressed:
-            self.button_2.setText(i)
+            self.gen_func = i
+
 
     def run2(self):
         i, okBtnPressed = QInputDialog.getItem(
@@ -114,73 +146,58 @@ class Example(QWidget):
             False
         )
         if okBtnPressed:
-            self.button_3.setText(i)
+            self.duration_tone = i
+            self.duration_tone = int(self.duration_tone)
 
     def keyPressEvent(self, event):
-        SAMPLE_RATE = 44100
-        #                      0       1       2      3        4       5       6
-        #                      до      ре      ми     фа       соль    ля      си
-        freq_array = np.array([261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88])
-        key_names = ['a', 's', 'd', 'f', 'g', 'h', 'j']
-        key_list = list(map(lambda x: ord(x), key_names))
-        key_dict = dict([(key, False) for key in key_list])
-
-        gen_func = np.sin
-        gen_func_list = (
-            (np.sin, ord('w')),
-            (square, ord('e')),
-            (saw, ord('r')),
-            (triangle, ord('t'))
-        )
-
         p = pa.PyAudio()
         stream = p.open(format=p.get_format_from_width(width=2),
                         channels=2,
                         rate=SAMPLE_RATE,
                         output=True)
-        self.duration_tone = 1 / 2.0
-        tones = generate_tones(self.duration_tone)
-        if event.key == ord('-'):
+        if event.key() == ord('-'):
             self.duration_tone /= 2
             print('duration_tone =', self.duration_tone)
-            tones = generate_tones(self.duration_tone)
-        if event.key == ord('='):
+            self.tones = generate_tones(self.duration_tone)
+        if event.key() == ord('='):
             self.duration_tone *= 2
             print('duration_tone =', self.duration_tone)
-            tones = generate_tones(self.duration_tone)
-        if event.key == ord('1'):
-            freq_array /= 2
-            print('freq_array =', freq_array)
-            tones = generate_tones(self.duration_tone)
-        if event.key == ord('2'):
-            freq_array *= 2
-            print('freq_array =', freq_array)
-            tones = generate_tones(self.duration_tone)
-        for (function, key) in gen_func_list:
-            if key == event.key:
+            self.tones = generate_tones(self.duration_tone)
+        if event.key() == ord('1'):
+            self.freq_array /= 2
+            print('freq_array =', self.freq_array)
+            self.tones = generate_tones(self.duration_tone)
+        if event.key() == ord('2'):
+            self.freq_array *= 2
+            print('freq_array =', self.freq_array)
+            self.tones = generate_tones(self.duration_tone)
+        for (function, key) in self.gen_func_list:
+            if event.key() == key:
                 print('gen_function =', function.__name__)
-                gen_func = function
-                tones = generate_tones(self.duration_tone)
-        for (index, key) in enumerate(key_list):
-            if event.key == key:
-                key_dict[key] = True
-        if event.type == Qt.KEYUP:
-            for (index, key) in enumerate(key_list):
-                if event.key == key:
-                    key_dict[key] = False
-        for (index, key) in enumerate(key_list):
-            if key_dict[key] is True:
-                stream.write(tones[index])
+                self.gen_func = function
+                self.tones = generate_tones(self.duration_tone)
+        for (index, key) in enumerate(self.key_list):
+            if event.key() == key:
+                stream.write(self.tones[index])
+
+
+
 
 
 if __name__ == '__main__':
-    duration_tone = 1 / 2.0
-    tones = generate_tones(duration_tone)
+    SAMPLE_RATE = 44100
+    #                      0       1       2      3        4       5       6
+    #                      до      ре      ми     фа       соль    ля      си
+    freq_array = np.array([261.63, 293.66, 329.63, 349.23, 392.00, 440.00, 493.88])
+    key_names = ['a', 's', 'd', 'f', 'g', 'h', 'j']
+    key_list = list(map(lambda x: ord(x), key_names))
+    key_dict = dict([(key, False) for key in key_list])
     p = pa.PyAudio()
     stream = p.open(format=p.get_format_from_width(width=2),
                     channels=2,
                     rate=SAMPLE_RATE,
                     output=True)
+
     app = QApplication(sys.argv)
     ex = Example()
     ex.show()
